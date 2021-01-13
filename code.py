150c097e01c0d9555569a575813cc243ac964bff
Marijn Doeve <marijn@doeve.me>
2021-01-13 10:16:40 +0100

    main

--- code.py
+++ code.py
@@ -5,13 +5,14 @@ def recursive_fibonacci(n):
         return(recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2))
 
 
-n_terms = 10
+if __name__ == "__main__":
+    n_terms = 10
 
 
-# check if the number of terms is valid
-if n_terms <= 0:
-    print("Please enter an integer")
-else:
-    print("Fibonacci sequence:")
-    for i in range(n_terms):
-        print(recursive_fibonacci(i))
+    # check if the number of terms is valid
+    if n_terms <= 0:
+        print("Please enter an integer")
+    else:
+        print("Fibonacci sequence:")
+        for i in range(n_terms):
+            print(recursive_fibonacci(i))
